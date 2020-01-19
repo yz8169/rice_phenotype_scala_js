7eea80441a7524be3ddb17a3fc433c42cf54b79e
yz <1454190131@qq.com>
2020-01-19 14:09:05 +0800

    水稻表型，scala.js版本，新增用户权限

--- server/app/test/Test.scala
+++ server/app/test/Test.scala
@@ -12,14 +12,16 @@ import utils.Implicits._
 object Test {
 
   def main(args: Array[String]): Unit = {
+    val runtime=Runtime.getRuntime
+//    runtime.cp
 
-    val parent = new File("G:\\rice_phenotype\\test")
-    val file=new File(parent,"外引种.xlsx")
-    val lines=Utils.xlsx2Lines(file)
-    val names=lines.headers.drop(12).dropRight(5)
-    println(names.map("\""+_+"\""))
-
-    println(Shared.localSamplePhenotypeNames.size)
+//    val parent = new File("G:\\rice_phenotype\\test")
+//    val file=new File(parent,"外引种.xlsx")
+//    val lines=Utils.xlsx2Lines(file)
+//    val names=lines.headers.drop(12).dropRight(5)
+//    println(names.map("\""+_+"\""))
+//
+//    println(Shared.localSamplePhenotypeNames.size)
 
 
 
